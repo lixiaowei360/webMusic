fbd287e1d08b9dc64b5227a89e6417dfc32b51e8
= <1039004153@qq.com>
2018-07-28 19:26:29 +0800

    Merge branch 'master' of https://github.com/lixiaowei360/webMusic
    # Conflicts:
    #	src/main/java/com/webMusic/common/controller/BaseController.java

--- src/main/java/com/webMusic/common/controller/BaseController.java
+++ src/main/java/com/webMusic/common/controller/BaseController.java
@@ -45,7 +45,7 @@ public class BaseController {
 	protected final static Logger logger = Logger.getLogger(BaseController.class);
 	protected Map<String, Object> resultMap = new LinkedHashMap<String, Object>();
 
-	public static String URL404 =  "/刘7fawfsfsadf404.html";
+	public static String URL404 =  "/刘7fawfsfsadf404.html ";
 
 	private final static String PARAM_PAGE_NO = "passfgeNo";
 
